1a8e26886098ca36af9e8191015a40d2b8e88193
Rafal Hirsz <hirszdesign@gmail.com>
2011-06-28 22:51:27 +0200

    Engine is a singleton.

--- include/Engine.h
+++ include/Engine.h
@@ -3,11 +3,22 @@
 
 namespace Blast {
     class Engine {
+	private:
+		Engine();
+		Engine(const Engine&);
+		Engine& operator=(const Engine&);
     public:
-        Engine();
-        
         void test();
+
+		static Engine& get() {
+			static Engine engine;
+			return engine;
+		}
     };
+
+	static Engine& engine() {
+		return Engine::get();
+	}
 }
 
 #endif /* end of include guard: ENGINE_H_1D9Q0055 */
